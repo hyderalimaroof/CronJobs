ca2fcb6ff78acc18d93f03e1a5a5d8aff9296ae5
Muhammad Hammad <hammadarshad834@gmail.com>
2022-03-10 12:42:44 +0500

    changes in setting file and create CI.yml file

--- cron_jobs/settings.py
+++ cron_jobs/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'cron_jobs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Cronjob',
-        'USER': 'hyder',
-        'PASSWORD': 'hyder',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'Cronjob',
+#         'USER': 'hyder',
+#         'PASSWORD': 'hyder',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 CACHES = {
     'default': {
